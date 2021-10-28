62a0b41d277246ab01cd98dbf048090c2401af1d
Andres Benavides <andresbemavidesmejia@gmail.com>
2021-10-28 01:57:12 -0500

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonAddComponent } from './counter/button-add/button-add.component';
+import { ButtonDisComponent } from './counter/button-dis/button-dis.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    ButtonAddComponent,
+    ButtonDisComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/counter/button-add/button-add.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button-add',
+  templateUrl: './button-add.component.html',
+  styleUrls: ['./button-add.component.css']
+})
+export class ButtonAddComponent {
+  @Input()
+  contador:number = 0;
+
+  @Output()
+  onAdd: EventEmitter<number> = new EventEmitter();
+
+  add(){
+    this.contador++;
+    this.onAdd.emit(this.contador);
+  }
+
+}
